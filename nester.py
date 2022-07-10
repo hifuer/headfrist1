9e1736e4da3b911ceda230ff8e0245260e1e6ce3
hifuer <hifuer@qq.com>
2022-07-10 12:57:19 +0800

    nester.py update

--- nester.py
+++ nester.py
@@ -2,14 +2,14 @@
         ["Graham Chapman",["Michael Palin","John Cleese",
                            "Terry Gilliam","Eric Idle","Terry Jones"]]]"""
 "定义方法"
-def print_lol(the_list，level):
+def print_lol(the_list,level):
     for each_item in the_list:
         "检查是否为list 列表"
         if isinstance(each_item,list):
             print_lol(each_item,level+1)
         else:
-        	for tab_stop in range(level):
-        		print("\t",end='')
+            for tab_stop in range(level):
+        	    print("\t",end='')
             print(each_item)
 
 """print_lol(movies)"""
